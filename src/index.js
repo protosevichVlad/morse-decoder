d3fe12fab997ece8b16a350add9b889a3600598f
Protosevich <protosevich2001@gmail.com>
2020-03-06 23:25:45 +0300

    feat: full solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+function multiSlice(str, number){
+    let array_elemnt_length_number = [];
+    while(str.length != 0){
+        array_elemnt_length_number.push(str.slice(0, number));
+        str = str.slice(number, str.length);
+    }
+    return array_elemnt_length_number;
+}
+
 function decode(expr) {
-    // write your solution here
+    let result = "";
+    let array_for_decode = multiSlice(expr, 10);
+    for(let i = 0; i < array_for_decode.length; i++){
+        if(array_for_decode[i] === '**********'){
+            result += ' ';
+            continue;
+        }
+        let symble_for_decode = multiSlice(array_for_decode[i], 2);
+        let result_symble = '';
+        for(let j = 0; j < 5; j++){
+            if (symble_for_decode[j] === '10') result_symble += '.';
+            if (symble_for_decode[j] === '11') result_symble += '-';
+        }
+        result += MORSE_TABLE[result_symble];
+    }
+    return result;
 }
 
 module.exports = {
